3d5170b812220b2d98712f362c4750bf28828239
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-22 14:39:28 +0300

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        # Constructor to initialize the Book object with title and page count
+        self.title = title
+        self._page_count = page_count  # Using an underscore to indicate it's a "private" attribute
+
+    @property
+    def page_count(self):
+        return self._page_count  # Getter method for page count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value  # Setter method for page count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+book = Book("And Then There Were None", 272)
+print(book.page_count)  # Output: 272
+print(book.title)  # Output: "And Then There Were None"
+book.page_count = 300  # Setting new page count
+book.page_count = "not an integer"  # This will print an error message
+book.turn_page()  # Output: "Flipping the page...wow, you read fast!"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = "New"  # Default condition is set to "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Update the condition after cobbling
+
+
+stan_smith = Shoe("Adidas", 9)
+print(stan_smith.brand)  # Output: "Adidas"
+print(stan_smith.size)
+stan_smith.size = 10  # Setting new size
+stan_smith.size = "not an integer"  # This will print an error message
+stan_smith.cobble()  # Output: "Your shoe is as good as new!"
+print(stan_smith.condition)  # Output: "New"
+stan_smith.cobble()  # Output: "Your shoe is as good as new!"
+print(stan_smith.condition)  # Output: "New"
